c1ad0cf82986086d8d32a4ae2ade82ac1e3fea9d
EmyBaby <emydiusmontes@gmail.com>
2022-11-03 09:10:43 -0700

    Added angular push if player rotates too far

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,20 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D body;
     private BoxCollider2D boxCollider;
+    private ConstantForce2D gravity;
+
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float speed; // Allows us to set speed within Unity while keeping it a private variable, for security reasons.
+
+    // Vector2 gravity = new Vector2(0, -9.8f);
 
     // Awake is called even if the script is disabled.
     private void Awake() {
         body = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        gravity = GetComponent<ConstantForce2D>();
+        // body.useGravity = false;
+        // LeftGravity();
     }
 
     // Start is called before the first frame update
@@ -24,9 +31,10 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     private void Update() {
+
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-
+        
         if (horizontalInput > 0.01f)
             transform.localScale = new Vector3(3, 3, 3);
         else if (horizontalInput < -0.01f)
@@ -35,11 +43,26 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space) && isGrounded()) {
             body.velocity = new Vector2(body.velocity.x, speed);
         }
+
+        // Helps rotate the player upright mid-air if it is rotating unsafely
+        // if (!isGrounded()) {
+            if (transform.rotation.eulerAngles.z > 30 && transform.rotation.eulerAngles.z < 180) {
+                Debug.Log("Rotation Detected");
+                transform.Rotate(0, 0, -0.4f);
+            } 
+            else if (transform.rotation.eulerAngles.z < 330 && transform.rotation.eulerAngles.z >= 180) {
+                transform.Rotate(0, 0, 0.4f);
+            }
+        // }
     }
 
     // Returns boolean based on if the player is grounded
     private bool isGrounded() {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
         return raycastHit.collider != null;
+    }
+
+    private void LeftGravity() {
+        // gravity = new Vector2(-9.8f, 0);
     }
 }
