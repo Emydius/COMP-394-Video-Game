e7d5583f7988bf72ed9e07ef3d066c4b36a31a32
Emydius Montes <emydiusmontes@gmail.com>
2022-12-14 21:11:15 -0600

    Added left bound to camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -19,7 +19,6 @@ public class CameraFollow : MonoBehaviour
         Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
         if (target.position.x >= -655.5453f) {
             transform.position = Vector3.Slerp(transform.position, newPos, followSpeed*Time.deltaTime);
-        // } else transform.position = new Vector3(-655.5453f, transform.position.y, transform.position.z);
         } else transform.position = Vector3.Slerp(transform.position, new Vector3(-655.5453f, newPos.y, newPos.z), followSpeed*Time.deltaTime);
     }
 }
